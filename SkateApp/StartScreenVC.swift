084687c6544a1ed85b0ef953f45de0175e062bf9
Daniel Reicher <reicher.dani@gmail.com>
2017-07-08 18:31:12 +0200

    unnötigen text gelöscht

--- SkateApp/StartScreenVC.swift
+++ SkateApp/StartScreenVC.swift
@@ -16,11 +16,6 @@ class StartScreenVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
     /*
     // MARK: - Navigation
