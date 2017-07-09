5178ba215a6a5c507637060db5a5c2673738ea51
Daniel Reicher <reicher.dani@gmail.com>
2017-07-09 23:33:26 +0200

    test

--- SkateApp/StartScreenVC.swift
+++ SkateApp/StartScreenVC.swift
@@ -18,7 +18,7 @@ class StartScreenVC: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
 
