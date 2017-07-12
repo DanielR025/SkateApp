e4f272f4d712e5cfad61b148c264d8b6d63dbb46
Daniel Reicher <reicher.dani@gmail.com>
2017-07-12 22:13:07 +0200

    TableView hinzugefügt

--- SkateApp/StartScreenVC.swift
+++ SkateApp/StartScreenVC.swift
@@ -1,36 +1,50 @@
 //
-//  StartScreenVC.swift
+//  StartscreenVC.swift
 //  SkateApp
 //
-//  Created by Daniel Reicher on 08.07.17.
+//  Created by Daniel Reicher on 12.07.17.
 //  Copyright © 2017 Daniel Reicher. All rights reserved.
 //
 
 import UIKit
 
-class StartScreenVC: UIViewController {
+class StartscreenVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    
+    @IBOutlet weak var TableView: UITableView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        TableView.delegate = self
+        TableView.dataSource = self
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-    */
 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        return UITableViewCell()
+    }
+    
+    
+    
+    
 }
-
